6654d1275cfb61142a0dbf978e431efc1375acc8
David King <davidgwking@gmail.com>
2014-10-21 22:36:50 -0500

    some documentation for the previous plumbing

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ func main() {
   // initialize router
   router := NewMethodRouter()
 
+  // register routes
   router.HandleFunc("GET", "/", HandleHelloWorld)
 
   // TODO: move to http.Server instantiation if we need TLS
--- router.go
+++ router.go
@@ -6,8 +6,11 @@ import (
   "github.com/gorilla/mux"
 )
 
+// A common type used to express what middleware and route handler function signatures look like
 type HttpHandler func(w http.ResponseWriter, r *http.Request)
 
+// The MethodRouter type provides a convenience wrapper around mux.Router that provides easy-to-use
+// http method-based routing.
 type MethodRouter struct {
   methods []string
   primaryRouter *mux.Router
@@ -25,11 +28,13 @@ func NewMethodRouter() *MethodRouter {
   return router
 }
 
+// Register a router. See mux.Router documentation for regexp path rules, etc.
 func (mr MethodRouter) HandleFunc(method string, path string, handleFunc HttpHandler) {
-  mr.subRouters[method].HandleFunc(path, DecorateMdw(handleFunc))
+  mr.subRouters[method].HandleFunc(path, DecoratorMdw(handleFunc))
   return
 }
 
+// Start server.
 func (mr MethodRouter) ListenAndServe(addr string) {
   log.Fatal(http.ListenAndServe(addr, mr.primaryRouter))
   return

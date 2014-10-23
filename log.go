76b3ff2566071bdf8242179d0ffe105044938d31
David King <davidgwking@gmail.com>
2014-10-22 22:18:55 -0500

    logger wrapper now stores ptr to library logger
    - logger wrapper now stores ptr to library logger

--- log.go
+++ log.go
@@ -14,13 +14,13 @@ const (
 // simple wrapper around the go-logging library that allows us to easily create child loggers
 // TODO: support filesystem logging
 type logger struct {
-  logging.Logger
+  *logging.Logger
   level int
 }
 
 // get a new server logger
 func NewServerLogger(logLevel int, logFormat string) *logger {
-  logger := &logger{*logging.MustGetLogger("server"), logLevel}
+  logger := &logger{logging.MustGetLogger("server"), logLevel}
 
   logging.SetFormatter(logging.MustStringFormatter(logFormat))
 
@@ -35,7 +35,7 @@ func (sl logger) Child(name string) (*logger, error) {
     return nil, err
   }
 
-  child := &logger{*libLogger, sl.level}
+  child := &logger{libLogger, sl.level}
   logging.SetLevel(logging.Level(sl.level), name)
   return child, nil
 }
